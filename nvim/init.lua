51372b9e662f068badd145d3ceb130c15b4f71b7
Mohammed Dadi <artofmohammed@gmail.com>
2026-08-12 21:17:02 +0100

    working config v002

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,20 +15,21 @@ vim.keymap.set('n', '<leader>j', '<c-w>j')
 vim.keymap.set('n', '<leader>k', '<c-w>k')
 vim.keymap.set('n', '<leader>h', '<c-w>h')
 vim.keymap.set('n', '<leader>l', '<c-w>l')
-
+vim.keymap.set('n', '<leader>Q', '<cmd>wqa<cr>', { noremap = true, silent = true })
 
 vim.keymap.set('n', '<leader>s', '<cmd>w<cr>', { noremap = true, silent = true })
 
 vim.keymap.set('n', '<leader>c', '"+Y', { noremap = true, silent = true })
 vim.keymap.set('v', '<leader>c', '"+y', { noremap = true, silent = true })
 
-
 vim.keymap.set('n', 'gp', '<cmd>telescope lsp_definitions<cr>', { desc = 'peek definition' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'next diagnostic' })
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'prev diagnostic' })
 vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'show diagnostic' })
 
-vim.keymap.set('n', '<leader>q', '<cmd>wqa<cr>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>q', '<cmd>wq<cr>', { noremap = true, silent = true })
+vim.keymap.set('t', '<leader>q', [[<C-\><C-n>:bd!<CR>]], { desc = 'Kill terminal buffer' })
+vim.keymap.set('n', '<leader>cl', '<cmd>q<cr>', { noremap = true, silent = true })
 
 vim.keymap.set('t', '<esc>', [[<c-\><c-n>]], { noremap = true, silent = true })
 vim.keymap.set('t', '<s-j>', [[<c-\><c-n><c-w>j]], { noremap = true, silent = true })
@@ -40,17 +41,43 @@ vim.keymap.set('n', '<leader>ch', ':nohl<cr>', { noremap = true, silent = true }
 vim.keymap.set('n', '<leader>jj', ':m .+1<cr>==', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>kk', ':m .-2<cr>==', { noremap = true, silent = true })
 
+vim.keymap.set('n', '<leader>rl', '<cmd>LivePreview start<cr>', { desc = 'start live preview' })
+vim.keymap.set('n', '<leader>rs', '<cmd>LivePreview stop<cr>', { desc = 'stop live preview' })
+
 vim.keymap.set('n', '<leader>e', '$') -- end of line
 vim.keymap.set('n', '<leader>b', '0') -- beginning of line
 
 vim.keymap.set('n', '<leader>fm', function() vim.lsp.buf.format { async = false } end, { desc = 'format file' })
 vim.keymap.set('n', '<leader>n', ':NvimTreeToggle<cr>', { desc = 'toggle file tree' })
 
+vim.fn.sign_define('DapBreakpoint', { text = '🔴', texthl = 'DapBreakpoint', linehl = 'DapBreakpoint', numhl = 'DapBreakpoint' })
+
+vim.fn.sign_define('DapStopped', { text = '🔴', texthl = 'DapStopped', linehl = 'DapStopped', numhl = 'DapStopped' })
+
+vim.keymap.set('n', '<leader>jr', function()
+  vim.cmd 'JavaBuildBuildWorkspace'
+  vim.cmd 'JavaRunnerRunMain'
+  -- give the terminal buffer a moment to open, then clear its scrollback
+  vim.defer_fn(function()
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+      if vim.bo[buf].buftype == 'terminal' then
+        local sb = vim.bo[buf].scrollback
+        vim.bo[buf].scrollback = 1
+        vim.bo[buf].scrollback = sb
+      end
+    end
+  end, 200) -- wait 200ms for nvim-java's terminal to open
+end, { desc = 'Java: Build + Run' })
+
 vim.opt.autowriteall = true
 
 vim.api.nvim_create_autocmd('focuslost', {
   pattern = '*',
   command = 'silent! wa',
+})
+
+vim.api.nvim_create_autocmd('TermOpen', {
+  callback = function(args) vim.keymap.set('n', '<leader>q', '<cmd>close<cr>', { buffer = args.buf, silent = true }) end,
 })
 
 vim.api.nvim_create_autocmd('bufleave', {
@@ -71,6 +98,15 @@ end)
 
 vim.api.nvim_create_autocmd('filetype', {
   pattern = 'go',
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+  end,
+})
+
+vim.api.nvim_create_autocmd('filetype', {
+  pattern = 'java',
   callback = function()
     vim.opt_local.tabstop = 4
     vim.opt_local.shiftwidth = 4
@@ -112,7 +148,7 @@ require('lazy').setup {
     -- add your plugins here
 
     { 'nvim-lua/plenary.nvim' }, -- none-ls depends on this
-    { 'nvimtools/none-ls.nvim' },
+    { 'nvimtools/none-ls.nvim', dependencies = { 'nvimtools/none-ls-extras.nvim' } },
     {
       'folke/tokyonight.nvim',
       lazy = false,
@@ -155,19 +191,60 @@ require('lazy').setup {
         })
       end,
     },
+    -- nvim v0.8.0
+    {
+      'kdheepak/lazygit.nvim',
+      lazy = true,
+      cmd = {
+        'LazyGit',
+        'LazyGitConfig',
+        'LazyGitCurrentFile',
+        'LazyGitFilter',
+        'LazyGitFilterCurrentFile',
+      },
+      -- optional for floating window border decoration
+      dependencies = {
+        'nvim-lua/plenary.nvim',
+      },
+      -- setting the keybinding for LazyGit with 'keys' is recommended in
+      -- order to load the plugin when the command is run for the first time
+      keys = {
+        { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+      },
+    },
     {
       'ThePrimeagen/vim-be-good',
     },
     {
+      'nvim-java/nvim-java',
+      config = function()
+        require('java').setup {
+          log = {
+            use_console = true,
+            use_file = true,
+            level = 'warn',
+            log_file = vim.fn.stdpath 'state' .. '/nvim-java.log',
+            max_lines = 1000,
+            show_location = false,
+          },
+        }
+        vim.lsp.enable 'jdtls'
+      end,
+    },
+    {
+
       'nvim-treesitter/nvim-treesitter',
       lazy = false,
       build = ':TSUpdate',
       config = function()
-        require('nvim-treesitter').setup {} -- no .configs, no .config
+        require('nvim-treesitter').setup {
+          ensure_installed = { 'python', 'lua', 'go', 'javascript', 'java' },
+          auto_install = true,
+        } -- no .configs, no .config
 
         -- enable treesitter highlighting per filetype, the new way
         vim.api.nvim_create_autocmd('FileType', {
-          pattern = { 'go', 'lua', 'python', 'javascript' },
+          pattern = { 'go', 'lua', 'python', 'javascript', 'typescript', 'java' },
           callback = function() vim.treesitter.start() end,
         })
       end,
@@ -242,7 +319,7 @@ require('lazy').setup {
       build = function()
         -- build the fuzzy matcher, optionally add a timeout to `pwait(timeout_ms)`
         -- you can use `gb` in `:Lazy` to rebuild the plugin as needed
-        require('blink.cmp').build():pwait()
+        require('blink.cmp').build():pwait(5000)
       end,
 
       ---@module 'blink.cmp'
@@ -263,7 +340,12 @@ require('lazy').setup {
         keymap = { preset = 'super-tab' },
 
         -- (Default) Only show the documentation popup when manually triggered
-        completion = { documentation = { auto_show = false } },
+        completion = {
+          documentation = { auto_show = false },
+          accept = {
+            resolve_timeout_ms = 400, -- default is short; raise it so imports aren't dropped
+          },
+        },
 
         -- (Default) list of enabled providers defined so that you can extend it
         -- elsewhere in your config, without redefining it, due to `opts_extend`
@@ -339,8 +421,8 @@ require('lazy').setup {
         vim.keymap.set('n', '<leader>2', function() harpoon:list():select(2) end)
         vim.keymap.set('n', '<leader>3', function() harpoon:list():select(3) end)
         vim.keymap.set('n', '<leader>4', function() harpoon:list():select(4) end)
-        vim.keymap.set('n', '<S-n>', function() harpoon:list():next { ui_nav_wrap = true } end)
-        vim.keymap.set('n', '<S-p>', function() harpoon:list():prev { ui_nav_wrap = true } end)
+        vim.keymap.set('n', '<C-n>', function() harpoon:list():next { ui_nav_wrap = true } end)
+        vim.keymap.set('n', '<C-p>', function() harpoon:list():prev { ui_nav_wrap = true } end)
       end,
     },
 
@@ -348,6 +430,94 @@ require('lazy').setup {
       'windwp/nvim-autopairs',
       event = 'InsertEnter',
       config = function() require('nvim-autopairs').setup() end,
+    },
+    {
+      'mfussenegger/nvim-dap',
+      dependencies = {
+        -- UI panels (variables, call stack, etc.)
+        { 'rcarriga/nvim-dap-ui', dependencies = { 'nvim-neotest/nvim-nio' } },
+        -- installs delve via Mason automatically
+        { 'jay-babu/mason-nvim-dap.nvim' },
+        -- pre-configures dap for Go so you don't wire delve manually
+        { 'leoluz/nvim-dap-go' },
+        -- shows variable values inline as virtual text while debugging
+        { 'theHamsta/nvim-dap-virtual-text' },
+
+        { 'mfussenegger/nvim-dap-python' },
+      },
+      config = function()
+        local dap = require 'dap'
+        local dapui = require 'dapui'
+
+        -- setup mason-nvim-dap: auto-install delve
+        require('mason-nvim-dap').setup {
+          ensure_installed = { 'delve', 'python', 'node2', 'java-debug' },
+          automatic_installation = true,
+          handlers = {}, -- required, even if empty
+        }
+
+        -- setup the UI
+        dapui.setup()
+        -- setup virtual text
+        require('nvim-dap-virtual-text').setup()
+        -- setup Go-specific dap config (wires delve for you)
+        require('dap-go').setup()
+
+        -- Python
+        local function get_python_path()
+          -- get the current working directory nvim was opened in
+          local cwd = vim.fn.getcwd()
+          local venv_python = cwd .. '/.venv/bin/python'
+
+          -- check if that file actually exists on disk
+          if vim.fn.filereadable(venv_python) == 1 then return venv_python end
+
+          -- fall back to a system python if no .venv is found
+          return '/opt/homebrew/opt/python@3.14/bin/python3.14'
+        end
+
+        require('dap-python').setup(get_python_path())
+
+        dap.listeners.after.event_initialized['dapui_config'] = function() dapui.open() end
+        dap.listeners.before.event_terminated['dapui_config'] = function() dapui.close() end
+        dap.listeners.before.event_exited['dapui_config'] = function() dapui.close() end
+
+        -- keymaps
+        vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, { desc = 'toggle breakpoint' })
+        vim.keymap.set('n', '<leader>dc', dap.continue, { desc = 'continue / start' })
+        vim.keymap.set('n', '<leader>do', dap.step_over, { desc = 'step over' })
+        vim.keymap.set('n', '<leader>di', dap.step_into, { desc = 'step into' })
+        vim.keymap.set('n', '<leader>du', dapui.toggle, { desc = 'toggle dap ui' })
+        vim.keymap.set('n', '<leader>dt', require('dap-go').debug_test, { desc = 'debug go test' })
+      end,
+    },
+    {
+      'folke/todo-comments.nvim',
+      dependencies = { 'nvim-lua/plenary.nvim' },
+      opts = {
+        -- keywords recognized and how to display them
+      },
+      config = function()
+        require('todo-comments').setup()
+
+        -- keymaps
+        vim.keymap.set('n', '<leader>td', '<cmd>TodoTelescope<cr>', { desc = 'todos' })
+        vim.keymap.set('n', ']t', function() require('todo-comments').jump_next() end, { desc = 'next todo' })
+        vim.keymap.set('n', '[t', function() require('todo-comments').jump_prev() end, { desc = 'prev todo' })
+      end,
+    },
+    {
+      'jpalardy/vim-slime',
+      lazy = false,
+      init = function()
+        vim.g.slime_target = 'tmux'
+        vim.g.slime_default_config = { socket_name = 'default', target_pane = '{last}' }
+        vim.g.slime_dont_ask_default = 1
+      end,
+    },
+    {
+      'brianhuster/live-preview.nvim',
+      cmd = { 'LivePreview' },
     },
   },
 
@@ -359,30 +529,53 @@ require('lazy').setup {
 }
 require('mason').setup()
 require('mason-lspconfig').setup {
-  ensure_installed = { 'gopls' },
+  ensure_installed = { 'gopls', 'jdtls', 'pyright' },
   automatic_installation = true,
 }
+-- Tell pyright to suggest unimported symbols AND add the import when accepted
+vim.lsp.config('pyright', {
+  settings = {
+    python = {
+      analysis = {
+        autoImportCompletions = true,
+      },
+    },
+  },
+})
+vim.lsp.enable 'pyright'
+
+-- Tell gopls to do the same for Go
+vim.lsp.config('gopls', {
+  settings = {
+    gopls = {
+      completeUnimported = true,
+      usePlaceholders = true,
+    },
+  },
+})
+vim.lsp.enable 'gopls'
+vim.lsp.enable 'pyright'
 local null_ls = require 'null-ls'
 
 null_ls.setup {
   sources = {
     -- Formatting
-    null_ls.builtins.formatting.gofmt, -- formats Go code (standard)
-    null_ls.builtins.formatting.goimports, -- formats + organises imports
+    null_ls.builtins.formatting.gofmt,
+    null_ls.builtins.formatting.goimports,
+    null_ls.builtins.formatting.prettier,
+    require 'none-ls.formatting.ruff', -- moved from builtins
+    require 'none-ls.diagnostics.ruff', -- moved from builtins
 
     -- Linting
-    -- null_ls.builtins.diagnostics.golangci_lint, -- runs golangci-lint
     null_ls.builtins.diagnostics.golangci_lint.with {
       condition = function(utils) return utils.root_has_file 'go.mod' end,
     },
-
-    null_ls.builtins.formatting.stylua, -- runs golangci-lint
+    null_ls.builtins.formatting.stylua,
     null_ls.builtins.diagnostics.staticcheck.with {
       condition = function(utils) return utils.root_has_file 'go.mod' end,
     },
   },
 }
-
 -- Format on save for Go files
 vim.api.nvim_create_autocmd('BufWritePre', {
   pattern = '*.go',
@@ -390,17 +583,41 @@ vim.api.nvim_create_autocmd('BufWritePre', {
 })
 
 -- New native Neovim 0.11 way
-local capabilities = require('blink.cmp').get_lsp_capabilities()
-vim.lsp.config('gopls', { capabilities = capabilities })
-vim.lsp.enable 'gopls'
 
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(args)
     local opts = { buffer = args.buf }
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
   end,
 })
+
+vim.g.slime_target = 'tmux'
+vim.g.slime_default_config = { socket_name = 'default', target_pane = '{last}' }
+vim.g.slime_dont_ask_default = 1
+
+vim.keymap.set('n', '<leader>r', function()
+  vim.cmd 'write'
+  local file = vim.fn.expand '%:p'
+  local ft = vim.bo.filetype
+  local base
+
+  if ft == 'python' then
+    base = 'python3 ' .. file
+  elseif ft == 'go' then
+    base = 'go run ' .. file
+  else
+    print('No run command set for filetype: ' .. ft)
+    return
+  end
+
+  local args = vim.fn.input 'Args: '
+  local cmd = base
+  if args ~= '' then cmd = cmd .. ' ' .. args end
+
+  vim.fn['slime#send'](cmd .. '\n')
+end)
 
 local builtin = require 'telescope.builtin'
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
